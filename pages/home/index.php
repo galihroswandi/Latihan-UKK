d104d50b4899eb1b9e3b91fa99f758479e032256
galihroswandi <galiroswandi25@gmail.com>
2023-02-28 16:22:24 +0700

    Update font

--- pages/home/index.php
+++ pages/home/index.php
@@ -8,7 +8,7 @@
             <h3>Kelola SPP </h3>
             <h1>Web App Pembayaran SPP</h1>
             <h2>SMK Muhammadiyah Tasikmalaya</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus dolorem esse velit totam, exercitationem quos ullam eaque ducimus ipsa debitis optio asperiores quasi officia natus libero incidunt tempora quas quaerat pariatur fuga. Culpa obcaecati at perferendis officia inventore nesciunt saepe?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sequi fuga qui molestias animi quas officia, veniam sit error reiciendis ratione porro explicabo recusandae rem velit reprehenderit fugiat assumenda esse?</p>
             <a href="?p=petugas">Learn More</a>
         </div>
     </div>
--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pembayaran SPP | <?php strtoupper($_GET['p']) ?></title>
+    <title>Pembayaran SPP | <?= strtoupper($_GET['p']) ?></title>
+
+    <!-- fonts google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="public/css/pages/<?= $_GET['p'] ?>.css">
 </head>
 
